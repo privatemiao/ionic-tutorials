76dd4433f2fb8d4901e574d831465f663ba9f7db
Mel Liu <79642742@qq.com>
2015-08-22 17:04:26 +0800

    a

--- chapter3/example10/www/js/app.js
+++ chapter3/example10/www/js/app.js
@@ -3,17 +3,38 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', [ 'ionic' ])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory
+		// bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('login', {
+		url : '.login',
+		templateUrl : 'templates/login.html',
+		controller : 'LoginController'
+	}).state('app', {
+		url : '/app',
+		templateUrl : 'template/app.html',
+		controller : 'AppCtroller'
+	});
+
+	$urlRouterProvider.otherwise('/login');
+})
+
+.controller('LoginController', function($scope) {
+
+}).controller('AppController', function($scope) {
+
+});
