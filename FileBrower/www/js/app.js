fa99fa9c4ee2c2b606ab4db7ab57fb573765d8bd
刘淼 <liumiao@liumiaos-Mac.local>
2015-09-08 15:15:37 +0800

    a

--- FileBrower/www/js/app.js
+++ FileBrower/www/js/app.js
@@ -1,84 +1,43 @@
-angular.module('starter', [ 'ionic' ])
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-	$ionicPlatform.ready(function() {
-		if (window.cordova && window.cordova.plugins.Keyboard) {
-			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-		}
-		if (window.StatusBar) {
-			StatusBar.styleDefault();
-		}
-	});
-}).controller('FileController', function($scope, $ionicPlatform, fileService) {
-	$ionicPlatform.ready(function() {
-		$scope.refresh = function() {
-			fileService.getEntriesAtRoot().then(function(result) {
-				$scope.files = result;
-				console.log('Files->', $scope.files);
-			}, function(error) {
-				console.error(error);
-			});
-		};
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.controller('FileController', function($ionicPlatform, $scope){
+  $ionicPlatform.ready(function(){
+    console.log('Ionic Device Ready');
 
-		$scope.getContent = function(path) {
-			fileService.getEntries(path).then(function(result) {
-				$scope.files = result;
-				$scope.files.unshift({
-					name : '[parent]'
-				});
-				fileService.getParentDirectory(path).then(function(result) {
-					result.name = '[parent]';
-					$scope.files[0] = result;
-				}, function(error) {
-					console.error(error);
-				});
-			}, function(error) {
-				console.error(error);
-			});
-		};
-	});
-}).factory('fileService', function($q) {
-	return {
-		getParentDirectory : function(path) {
-			var deferred = $q.defer();
-			window.resolveLocalFileSystemURI(path, function(fileSystem) {
-				fileSystem.getParent(function(result) {
-					deferred.resolve(result);
-				}, function(error) {
-					deferred.reject(error);
-				});
-			}, function(error) {
-				deferred.reject(error);
-			});
-			return deferred.promise;
-		},
-		getEntriesAtRoot : function() {
-			var deferred = $q.defer();
-			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
-				var directoryReader = fileSystem.root.createReader();
-				directoryReader.readEntries(function(entries) {
-					deferred.resolve(entries);
-				}, function(error) {
-					deferred.reject(error);
-				});
-			}, function(error) {
-				deferred.reject(error);
-			});
-			return deferred.promise;
-		},
-		getEntries : function(path) {
-			var deferred = $q.defer();
-			window.resolveLocalFileSystemURI(path, function(fileSystem) {
-				var directoryReader = fileSystem.createReader();
-				directoryReader.readEntries(function(entries) {
-					deferred.resolve(entries);
-				}, function(error) {
-					deferred.reject(error);
-				});
-			}, function(error) {
-				deferred.reject(error);
-			});
-			return deferred.promise;
-		}
-	};
+    $scope.echo = function(){
+      console.log('Welcome');
+      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+        var directory = fileSystem.root.createReader();
+        directory.readEntries(function(entries){
+          var buffer = [];
+          entries.forEach(function(entry){
+            console.log('Name->', entry.name);
+            buffer.push(entry.name);
+          })
+          alert(buffer.join(' | '));
+        }, function(error){
+        console.error(error);
+      });
+      }, function(error){
+        console.error(error);
+      });
+    };
+  });
 })
