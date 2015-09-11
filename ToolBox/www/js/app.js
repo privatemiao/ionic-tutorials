5d5b61c02ae76be6c74af5202423eaf3036c2ab6
Mel Liu <privatemiao@gmail.com>
2015-09-11 15:22:36 +0800

    add ToolBox Project

---
+++ ToolBox/www/js/app.js
@@ -0,0 +1,57 @@
+angular.module('generic', [ 'ionic' ])
+
+.run(function($ionicPlatform) {
+	$ionicPlatform.ready(function() {
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
+}).config([ '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('tab', {
+		url : '/tab',
+		abstract : true,
+		templateUrl : 'partial/tabs.html'
+	}).state('tab.photo', {
+		url : '/photo',
+		views : {
+			'tab-photo' : {
+				templateUrl : 'partial/tab-photo.html'
+			}
+		}
+	}).state('tab.photo-upload', {
+		url : '/photo/upload',
+		views : {
+			'tab-photo' : {
+				templateUrl : 'partial/tab-photo-upload.html'
+			}
+		}
+	}).state('tab.photo-view', {
+		url : '/photo/view',
+		views : {
+			'tab-photo' : {
+				templateUrl : 'partial/tab-photo-view.html'
+			}
+		}
+	})
+
+	.state('tab.contact', {
+		url : '/contact',
+		views : {
+			'tab-contact' : {
+				templateUrl : 'partial/tab-contact.html'
+			}
+		}
+	}).state('tab.setting', {
+		url : '/setting',
+		views : {
+			'tab-setting' : {
+				templateUrl : 'partial/tab-setting.html'
+			}
+		}
+	})
+
+	$urlRouterProvider.otherwise('/tab/photo');
+} ])
