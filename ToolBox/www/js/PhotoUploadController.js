4130e3a1c7faa58deef7aacb6aa88e2b10a2e6eb
Mel Liu <privatemiao@gmail.com>
2015-09-13 15:07:22 +0800

    a

--- ToolBox/www/js/PhotoUploadController.js
+++ ToolBox/www/js/PhotoUploadController.js
@@ -11,11 +11,30 @@ angular.module('generic.controllers', []).controller('PhotoUploadController',
 							return;
 						}
 						navigator.notification.confirm('确定要上传 ' + $scope.photos.length + ' 张照片吗？', function(result) {
-							$scope.photos.forEach(function(photo) {
+							if (result != 1){
+								return;
+							}
+							
+							var uri = encodeURI('http://192.168.45.94:8080/upload');
+							
+							$scope.photos.forEach(function(photo){
 								resolveLocalFileSystemURL(photo, function(fileEntry) {
-									console.log('FileEntry->', fileEntry);
+									fileEntry.file(function(file){
+										var ft = new FileTransfer();
+										ft.upload(file.localURL, uri, function(response){
+											console.log('Success->', response);
+										}, function(error){
+											console.log('Error->', error);
+										}, {
+											fileKey : 'file',
+											fileName : file.name,
+											mimeType : file.type,
+											params : {name : file.name}
+										});
+									});
 								});
 							});
+							
 						}, '上传照片', [ '确定', '取消' ])
 					}, (function() {
 						$scope.photos = [];
