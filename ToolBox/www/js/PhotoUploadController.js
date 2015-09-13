15151528c3d85b5332a857cbff7029c06fece25d
Mel Liu <privatemiao@gmail.com>
2015-09-13 22:08:53 +0800

    a

--- ToolBox/www/js/PhotoUploadController.js
+++ ToolBox/www/js/PhotoUploadController.js
@@ -4,37 +4,25 @@ angular.module('generic.controllers', []).controller('PhotoUploadController',
 				$scope.photos = [];
 				$scope.upload = function() {
 					$scope.photos = [];
-					
+
 					PhotoUploadService.getAllPhotos(function() {
 						if ($scope.photos.length == 0) {
 							$cordovaToast.show('没有找到照片！', 'long', 'center');
 							return;
 						}
 						navigator.notification.confirm('确定要上传 ' + $scope.photos.length + ' 张照片吗？', function(result) {
-							if (result != 1){
+							if (result != 1) {
 								return;
 							}
-							
-							var uri = encodeURI('http://192.168.45.94:8080/upload');
-							
-							$scope.photos.forEach(function(photo){
-								resolveLocalFileSystemURL(photo, function(fileEntry) {
-									fileEntry.file(function(file){
-										var ft = new FileTransfer();
-										ft.upload(file.localURL, uri, function(response){
-											console.log('Success->', response);
-										}, function(error){
-											console.log('Error->', error);
-										}, {
-											fileKey : 'file',
-											fileName : file.name,
-											mimeType : file.type,
-											params : {name : file.name}
-										});
-									});
+
+							$scope.photos.forEach(function(photo) {
+								PhotoUploadService.upload(photo).then(function(success) {
+									console.log(success);
+								}, function(error) {
+									console.log(error);
 								});
 							});
-							
+
 						}, '上传照片', [ '确定', '取消' ])
 					}, (function() {
 						$scope.photos = [];
--- ToolBox/www/js/PhotoUploadService.js
+++ ToolBox/www/js/PhotoUploadService.js
@@ -1,4 +1,6 @@
 angular.module('generic.services', []).factory('PhotoUploadService', [ '$q', '$ionicLoading', '$timeout', function($q, $ionicLoading, $timeout) {
+	var uri = encodeURI('http://192.168.8.100:8080/upload');
+	
 	return {
 		getAllPhotos : function(callBack, photos) {
 			photos;
@@ -15,6 +17,30 @@ angular.module('generic.services', []).factory('PhotoUploadService', [ '$q', '$i
 					}, 500);
 				}
 			});
+		},
+		upload : function(photo) {
+			var deferred = $q.defer();
+			resolveLocalFileSystemURL(photo, function(fileEntry) {
+				fileEntry.file(function(file) {
+					var ft = new FileTransfer();
+					ft.upload(file.localURL, uri, function(response) {
+//						console.log('Success->', response);
+						deferred.resolve(response);
+					}, function(error) {
+//						console.log('Error->', error);
+						deferred.reject(error);
+					}, {
+						fileKey : 'file',
+						fileName : file.name,
+						mimeType : file.type,
+						params : {
+							name : file.name
+						},
+						date : file.lastModified
+					});
+				});
+			});
+			return deferred.promise;
 		}
 	};
 } ])
